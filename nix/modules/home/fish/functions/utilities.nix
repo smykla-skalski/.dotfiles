204112842e9439af6a34ab3db13ee78e5d2a4936
Bart Smykla <bartek@smykla.com>
2025-12-03 19:15:53 +0100

    feat(fish): add custom window title function
    Add `fish_title` function to display concise window titles with last two
    path components and current command. Skips title updates for interactive
    apps that manage their own titles (editors, TUIs, etc.).
    
    Format: "path ~ command" or just "path" when no command is running.
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- nix/modules/home/fish/functions/utilities.nix
+++ nix/modules/home/fish/functions/utilities.nix
@@ -40,5 +40,46 @@
         ln -sf $argv[1] $argv[2]
       '';
     };
+
+    fish_title = {
+      description = "Custom window title format";
+      body = ''
+        # Skip title updates for interactive apps that manage their own titles
+        set -l command (status current-command)
+        if test "$command" = fish
+          set command
+        end
+
+        # Use argv if provided (e.g., from fg command)
+        if set -q argv[1]
+          set command $argv[1]
+        end
+
+        # Don't set title for apps that control their own (editors, TUIs, etc)
+        # They'll set it themselves and we'll reset it when they exit
+        set -l skip_apps claude vim nvim emacs nano less more man ssh
+        if contains "$command" $skip_apps
+          return
+        end
+
+        # Get the last two path components for cleaner display
+        set -l pwd_parts (string split / (pwd))
+        set -l num_parts (count $pwd_parts)
+        set -l path
+
+        if test $num_parts -ge 2
+          set path "$pwd_parts[-2]/$pwd_parts[-1]"
+        else
+          set path $pwd_parts[-1]
+        end
+
+        # Format: "path ~ command" or just "path" if no command
+        if test -n "$command"
+          echo "$path ~ $command"
+        else
+          echo "$path"
+        end
+      '';
+    };
   };
 }
